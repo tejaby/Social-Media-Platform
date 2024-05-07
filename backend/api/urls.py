b587ddb86f73171e67a619ef0f110133bf9088d7
tejaby <yostintejaxun20@gmail.com>
2024-05-07 05:37:26 -0600

    add custom views for authentication with JWT

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -22,17 +22,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
+
+from apps.authentication.views import CustomTokenObtainPairView, CustomTokenRefreshView, CustomTokenVerifyView, CustomLogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/token/login/', CustomTokenObtainPairView.as_view(), name='login'),
+    path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='refresh'),
+    path('api/token/verify/', CustomTokenVerifyView.as_view(), name='verify'),
+    path('api/token/logout/', CustomLogoutView.as_view(), name='logout'),
     path('api/', include('apps.post.urls')),
     path('api/', include('apps.user.urls')),
 ]
--- backend/apps/authentication/views.py
+++ backend/apps/authentication/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.generics import GenericAPIView
+
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
+from apps.user.api.serializers import UserListSerializer
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        if not request.data.get('username') or not request.data.get('password'):
+            return Response({'error': 'se requiere nombre de usuario y contraseña'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(request=request,  username=request.data.get(
+            'username'), password=request.data.get('password'))
+
+        if user is None:
+            return Response({'error': 'no se encontró ninguna cuenta activa con las credenciales proporcionadas'}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user_serializer = UserListSerializer(user)
+
+        return Response({'message': 'inicio de sesión exitosamente', 'token': serializer.validated_data, 'user': user_serializer.data}, status=status.HTTP_200_OK)
+
+
+class CustomTokenRefreshView(TokenRefreshView):
+    pass
+
+
+class CustomTokenVerifyView(TokenVerifyView):
+    pass
+
+
+class CustomLogoutView(GenericAPIView):
+    def post(self, request, *args, **kwargs):
+        if not request.data.get('refresh'):
+            return Response({'error': 'se requiere el token de actualización'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            token = RefreshToken(request.data.get('refresh'))
+            token.blacklist()
+            return Response({"message": "cierre de sesión exitoso"}, status=status.HTTP_200_OK)
+        except TokenError:
+            return Response({"error": "Token de refresco inválido"}, status=status.HTTP_400_BAD_REQUEST)
