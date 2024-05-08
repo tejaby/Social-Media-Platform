b5ea2a1c1305679b7f69758a5d2382d3224d6e4e
tejaby <yostintejaxun20@gmail.com>
2024-05-08 13:43:08 -0600

    update post services

--- frontend/src/services/post.js
+++ frontend/src/services/post.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const urlPosts = "http://localhost:8000/api/posts/";
 
+const POST_API_BASE_URL = "http://localhost:8000/api/";
+
 export const getPosts = async (userToken) => {
   try {
     const response = await axios.get(`${urlPosts}`, {
@@ -39,5 +41,47 @@ export const user_posts = async (userToken) => {
     return posts;
   } catch (e) {
     throw e.response;
+  }
+};
+
+export const createPostService = async (data, access) => {
+  try {
+    const response = await axios.post(`${POST_API_BASE_URL}posts/`, data, {
+      headers: {
+        Authorization: `Bearer ${access}`,
+      },
+    });
+    return response.data;
+  } catch (err) {
+    console.error("Error creating post:", err);
+    throw err.response;
+  }
+};
+
+export const listPostService = async (access) => {
+  try {
+    const response = await axios.get(`${POST_API_BASE_URL}posts/`, {
+      headers: {
+        Authorization: `Bearer ${access}`,
+      },
+    });
+    return response.data;
+  } catch (err) {
+    console.error("Error listing posts:", err);
+    throw err.response;
+  }
+};
+
+export const getPostService = async (access) => {
+  try {
+    const response = await axios.get(`${POST_API_BASE_URL}posts/`, {
+      headers: {
+        Authorization: `Bearer ${access}`,
+      },
+    });
+    return response.data;
+  } catch (err) {
+    console.error("Error getting post:", err);
+    throw err.response;
   }
 };
